95b448f1c87c9e1ce63f8fb90d1df1249ed9c47e
Roma Ferre <romaferre@gmail.com>
2013-06-18 16:25:36 +0200

    3

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = 15000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
